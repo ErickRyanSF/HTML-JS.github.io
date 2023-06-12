1cba9af25b7779c27c4935b3aa8e1b4d48acba81
ErickRyanSF <127400109+ErickRyanSF@users.noreply.github.com>
2023-06-11 22:53:42 -0300

    Add files via upload

--- html com js/js/script.js
+++ html com js/js/script.js
@@ -2,6 +2,6 @@ const frm = document.querySelector("input")
 const resp = document.querySelector("h3")
 
 function mostrar(){
-  const livro = frm.value 
-  resp.innerText = "Seu livro favorito é " + livro + "? Tem bom gosto!!!"
+  const nome = frm.value 
+  resp.innerText = "Sua musica favorita é " + nome + "? Ótima música!!!"
 }
